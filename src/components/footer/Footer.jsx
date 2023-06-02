4846891092ae6fee5dbabbb58a03d099ce19b961
Jez <jeromedcarter@yahoo.co.uk>
2023-06-02 09:32:30 +0100

    Link amended

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
     </ul>
 
     <div className="footer__socials">
-      <a href="https://faceook.com"><FaFacebookF/></a>
+      <a href="https://facebook.com"><FaFacebookF/></a>
       <a href="https://instagram.com"><FiInstagram/></a>
       <a href="https://twitter.com"><IoLogoTwitter/></a> 
     </div>
